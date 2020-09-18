efdc10f5a2d7e26293534af833e366ee7c14028c
dhanushka-gayashan <dhanushka.kohombange@tenzing.co.nz>
2020-09-18 17:14:38 +1200

    kafka cusomise connector

---
+++ 02-Stephane-Maarek-Learn-Apache-Kafka-Connect/07-Write-Customise-Kafka-Connector/kafka-github-source-connector/README.md
@@ -0,0 +1,7 @@
+## How to run the connector
+
+`docker pull landoop/fast-data-dev`
+
+`docker-compose up kafka-cluster`
+
+`./run.sh`
--- 02-Stephane-Maarek-Learn-Apache-Kafka-Connect/07-Write-Own-Kafka-Connector/kafka-github-connector/src/test/java/com/github/kdg/DocumentationTest.java
+++
@@ -1,7 +0,0 @@
-package com.github.kdg;
-
-import com.github.jcustenborder.kafka.connect.utils.BaseDocumentationTest;
-
-public class DocumentationTest extends BaseDocumentationTest {
-
-}
